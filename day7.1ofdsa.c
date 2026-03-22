d7306cc73b5c5e6fe282d1c22a13b354ef384480
Khushbu Chauhan <khushbuchauhan25042007@gmail.com>
2026-03-22 21:54:10 +0530

    day7done

---
+++ day7.1ofdsa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fib(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    int result = fib(n);
+    printf("The %d-th Fibonacci number is: %d\n", n, result);
+    
+    return 0;
+}
