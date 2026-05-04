acde10c367df2826feef3f0b94895befdf16b722
Khushbu Chauhan <khushbuchauhan25042007@gmail.com>
2026-05-04 15:31:09 +0530

    day51done

---
+++ Day51_Q102.c
@@ -0,0 +1,106 @@
+/*Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        if (p->val < root->val && q->val < root->val)
+            root = root->left;
+        else if (p->val > root->val && q->val > root->val)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL)
+        return newNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+struct TreeNode* search(struct TreeNode* root, int val) {
+    if (root == NULL || root->val == val)
+        return root;
+
+    if (val < root->val)
+        return search(root->left, val);
+    else
+        return search(root->right, val);
+}
+
+int main() {
+    int n, val;
+    struct TreeNode* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    int p_val, q_val;
+    scanf("%d %d", &p_val, &q_val);
+
+    struct TreeNode* p = search(root, p_val);
+    struct TreeNode* q = search(root, q_val);
+
+    struct TreeNode* lca = lowestCommonAncestor(root, p, q);
+
+    if (lca != NULL)
+        printf("%d\n", lca->val);
+
+    return 0;
+}
