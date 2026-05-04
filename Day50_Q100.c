b4bc874d95b803df975628af6f737943c4908b14
Khushbu Chauhan <khushbuchauhan25042007@gmail.com>
2026-05-04 15:30:40 +0530

    day50done

---
+++ Day50_Q100.c
@@ -0,0 +1,93 @@
+/*You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL) return createNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (root == NULL || root->val == val)
+        return root;
+
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else
+        return searchBST(root->right, val);
+}
+
+void preorder(struct TreeNode* root) {
+    if (root == NULL) return;
+
+    printf("%d ", root->val);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    int n, x, val;
+    struct TreeNode* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &val);
+
+    struct TreeNode* result = searchBST(root, val);
+
+    if (result == NULL)
+        printf("[]");
+    else
+        preorder(result);
+
+    return 0;
+}
